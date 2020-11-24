8c611e560d7629c0439dc346a2a4991020612d4f
cliff538 <cliff538@yahoo.com>
2020-11-24 14:40:51 -0700

    last cleanup

---
+++ 21_stock_picker_milestone_project.py
@@ -0,0 +1,65 @@
+# stock data from account at https://api.tiingo.com/account/profile api=646d358e70fa9664e2795e73e1ce4ce4c13b5266
+
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import pandas_datareader.data as web
+from datetime import datetime
+import pandas as pd
+
+app = dash.Dash()
+
+
+app.layout = html.Div([
+    html.H1('Stock Ticker Dashboard'),
+    html.Div([html.H3('Enter a stock symbol:', style={'paddingRight': '30px'}),
+              dcc.Input(id='my_stock_picker',
+                        value='TSLA',
+                        style={'fontSize': 24, 'width': 75}
+                        )], style={'display': 'inline-block', 'verticalAlign': 'top'}),
+    html.Div([html.H3('Select a start and end date:'),
+              dcc.DatePickerRange(id='my_date_picker',
+                                  min_date_allowed=datetime(1960, 1, 1),
+                                  max_date_allowed=datetime.today(),
+                                  start_date=datetime(2020, 1, 1),
+                                  end_date=datetime.today()
+                                  )
+              ], style={'display': 'inline-block'}),
+    html.Div([
+        html.Button(id='submit-button',
+                        n_clicks= 0,
+                        children= 'Submit',
+                        style={'fontSize': 24, 'marginLeft': '30px'}
+                    )
+    ], style={'display': 'inline-block'}),
+
+    dcc.Graph(id='my_graph',
+              figure={'data': [
+                  {'x': [1, 2], 'y': [3, 1]}
+              ], 'layout': {'title': 'Default Title'}})
+
+])
+
+
+@app.callback(Output('my_graph', 'figure'),
+              [Input('submit-button', 'n_clicks')],
+              [State('my_stock_picker', 'value'),
+               State('my_date_picker', 'start_date'),
+               State('my_date_picker', 'end_date')
+               ])
+def update_graph(n_clicks, stock_ticker, start_date, end_date):
+    start = datetime.strptime(start_date[:10], '%Y-%m-%d')
+    end = datetime.strptime(end_date[:10], '%Y-%m-%d')
+    df = web.get_data_tiingo(stock_ticker, start, end, api_key='646d358e70fa9664e2795e73e1ce4ce4c13b5266')
+    fig = {'data': [
+        {'x': df.index.get_level_values(1),
+         'y': df.close}
+    ], 'layout': {'title': stock_ticker}
+    }
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server()
